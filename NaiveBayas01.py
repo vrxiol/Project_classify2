551632064634d449d156f8dd95c95764ab70a361
vrxiol <46449646+vrxiol@users.noreply.github.com>
2025-03-18 12:05:56 +0700

    .

--- NaiveBayas01.py
+++ NaiveBayas01.py
@@ -4,28 +4,28 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 
 Heart=pd.read_csv('./data/Heart.csv')
-X =Heart.drop(columns=['HeartDisease'])
+x =Heart.drop(columns=['HeartDisease'])
 y =Heart.HeartDisease
 
 model = GaussianNB()
-model.fit(X, y)
+model.fit(x, y)
 
 # ตั้งค่าหน้าเว็บ Streamlit
-st.title("Naïve Bayes Classifier - Iris Dataset")
+st.title("Naïve Bayes Classifier - ตรวจสอบโรคหัวใจ")
 st.write("ป้อนคุณสมบัติของดอกไม้เพื่อทำนายประเภท")
 
 # รับค่าจากผู้ใช้ผ่าน slider
 A1 = st.number_input("กรุณาเลือกข้อมูลอายุ")
 A2 = st.number_input("กรุณาเลือกเพศชาย=1 หญิง=0")
-A3 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก")
-A4 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก1")
-A5 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก2")
-A6 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก3")
-A7 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก4")
-A8 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก5")
-A9 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก6")
-A10 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก7")
-A11 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก8")
+A3 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก3")
+A4 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก4")
+A5 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก5")
+A6 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก6")
+A7 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก7")
+A8 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก8")
+A9 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก9")
+A10 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก10")
+A11 = st.number_input("กรุณาเลือกข้อมูลของเจ็บหน้าอก11")
 
 # สร้างอาร์เรย์ข้อมูลจากค่าที่ป้อน
 input_data = np.array([[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11]])
@@ -40,5 +40,5 @@ st.write(f"ชนิดของดอกไม้ที่คาดการณ
 
 # แสดงความน่าจะเป็นของแต่ละคลาส
 st.subheader("ความน่าจะเป็นของแต่ละประเภท:")
-df_proba = pd.DataFrame(prediction_proba, columns=Heart.HeartDisease)
+df_proba = pd.DataFrame(prediction_proba, columns=['0','1'])
 st.dataframe(df_proba.style.format("{:.2%}"))
